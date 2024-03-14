0cafc3480e4fcf4163f59e97039efec90c3f7333
pawel_kajdas <pawel.kajdas@machineportal.com>
2024-03-14 13:09:11 +0100

    feat: main photo - PO

--- routes/portal/api.php
+++ routes/portal/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MainPhoto\PO\MainPhotoController;
+use App\Http\Controllers\User\UserAccountController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -7,4 +9,11 @@ use Illuminate\Support\Facades\Route;
 | Actions
 |--------------------------------------------------------------------------
 */
-Route::get('verifyToken/{token}', [\App\Http\Controllers\User\UserAccountController::class, 'verifyRegisterToken'])->name('verifyToken');
+Route::get('verifyToken/{token}', [UserAccountController::class, 'verifyRegisterToken'])->name('verifyToken');
+
+/*
+|--------------------------------------------------------------------------
+| Main photo
+|--------------------------------------------------------------------------
+*/
+Route::apiResource('mainPhotos', MainPhotoController::class)->only(['index']);
