bda404450aa080646fa6728b3754cc6840f6f53f
pawel_kajdas <pawel.kajdas@machineportal.com>
2024-11-26 18:35:46 +0100

    fix: migrate

--- database/migrations/2025_03_13_144201_create_promotions_table.php
+++ database/migrations/2025_03_13_144201_create_promotions_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('news', function (Blueprint $table) {
+        Schema::create('promotions', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('url');
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('news');
+        Schema::dropIfExists('promotions');
     }
 };
