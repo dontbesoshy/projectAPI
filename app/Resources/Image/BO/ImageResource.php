7cb1cc3e3965898582f8a912be442a137ef8e2e8
pawel_kajdas <pawel.kajdas@machineportal.com>
2024-12-17 19:18:38 +0100

    feat: resource

--- app/Resources/Image/BO/ImageResource.php
+++ app/Resources/Image/BO/ImageResource.php
@@ -16,6 +16,7 @@ class ImageResource extends BasicResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'updatedAt' => $this->updated_at,
         ];
     }
 }
